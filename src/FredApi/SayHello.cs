83ff4f0e45c3ef0e82852041e9899f2e785a5ee1
Maximilian Gobbel <40639978+maex0@users.noreply.github.com>
2021-05-20 12:01:53 +0200

    Update SayHello.cs

--- src/FredApi/SayHello.cs
+++ src/FredApi/SayHello.cs
@@ -27,7 +27,7 @@ namespace FredApi
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+                : $"Hello, {name} and TH Rosenheim 😇";
 
             return new OkObjectResult(responseMessage);
         }
